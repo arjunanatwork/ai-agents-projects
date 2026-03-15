8b882ceb17d8cf588a0e103c3b589a2fe825408e
Arjunan Ravi <arjunanatwork@gmail.com>
2026-03-15 19:33:15 +0400

    Integrate Ollama-based agent and Gradio UI for interactive chat functionality
    - Replace Google Generative AI with Ollama as the agent's base model.
    - Add Gradio-powered user interface for real-time chatbot access.
    - Implement SQLite checkpointing for chat history management.

--- simple_ai_agent_with_gradio/main.py
+++ simple_ai_agent_with_gradio/main.py
@@ -1,14 +1,16 @@
+import uuid
 from datetime import datetime
 
 from dotenv import load_dotenv
-import os
 
 from langchain.agents import create_agent
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
+import gradio as gr
+
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
 load_dotenv()
-
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 def get_date():
     """
@@ -16,14 +18,36 @@ def get_date():
     """
     return datetime.now().strftime("%Y-%m-%d")
 
+conn = sqlite3.connect('chat_memory.db', check_same_thread=False)
+checkpointer = SqliteSaver(conn)
+
 system_prompt = """
-Use the get_date function to get the current date.
+You are a helpful assistant.
+Answer all user's question
+Use the get_date function if the user asks about today's date.
 """
 
-llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview")
+llm = ChatOllama(model="qwen3.5:4b")
 
-agent =create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
-user_question = input("Enter your question: ")
-response = agent.invoke({"messages": [{ "role": "user", "content": user_question}]})
+agent =create_agent(
+    model=llm,
+    tools=[get_date],
+    system_prompt=system_prompt,
+    checkpointer=checkpointer)
 
-print(response['message'][-1].content[0]['text'])
+def chat(message, history, thread_id):
+    config = {"configurable":{"thread_id": thread_id}}
+    response = agent.invoke(
+        {"messages": [{"role": "user", "content": message}]},
+        config
+    )
+    last_response = response["messages"][-1].content
+    return last_response
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("# AI Chatbot")
+    thread_id = gr.State(value=lambda: str(uuid.uuid4()))
+    gr.ChatInterface(fn=chat, additional_inputs=[thread_id], title="Chat with AI")
+
+demo.launch(share=True)
